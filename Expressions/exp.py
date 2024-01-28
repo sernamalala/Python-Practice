6a123830137d19f489cac8ded253e90e10cf2e4b
sernamalala <sernaprograms@gmail.com>
2024-01-28 23:04:28 +0200

    Python arithmetic expressions

---
+++ Expressions/exp.py
@@ -0,0 +1,35 @@
+# length = int(input("Enter the length of the rectangle: "))
+# breadth = int(input("Enter the breadth of the rectangle: "))
+
+# area = length*breadth
+# print(f"The area is {area} cm^2")
+##########################
+#Area of a triangle
+#############################
+# base = int(input("Enter the base of the triangle: "))
+# height = int(input("Enter the height of the triangle: "))
+
+# area = (1/2)*(base)*(height)
+
+# print(f"The area is {area} cm^2")
+######################
+# Area of a rhombus
+####################
+
+# a = int(input("Enter value of side a of the rhombus: "))
+# b = int(input("Enter value of side b of the rhombus: "))
+# height = int(input("Enter the height of the rhombus: "))
+
+# area = (1/2)*(a+b)*(height)
+
+# print(f"The area is {area} cm^2")
+
+#Displacement formula
+
+final_velocity = int(input("Enter value of the final velocity [v]: "))
+initial_velocity = int(input("Enter value of the initial velocity [v]: "))
+acceleration = int(input("Enter the value of acceleration: "))
+
+displacement = ((final_velocity**2)-(initial_velocity**2))/(2*acceleration)
+
+print(f"The displacement is {displacement} m")
