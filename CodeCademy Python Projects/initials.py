705d36ad52b08bf273b422105a62182726a051d8
sernamalala <sernaprograms@gmail.com>
2024-02-04 20:26:48 +0200

    Python exercises - codecademy

---
+++ CodeCademy Python Projects/initials.py
@@ -0,0 +1,10 @@
+print(" SSS  M   M")
+print("S   S MM MM")
+print("S     MM MM")
+print(" SSS  M M M")
+print("    S M    M")
+print("S   S M    M")
+print(" SSS  M    M")
+
+# Serna Malala
+# I am a python programmer
