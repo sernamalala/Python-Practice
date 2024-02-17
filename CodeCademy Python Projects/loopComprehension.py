bbf165e0d6878874c5d4204815d37459b5726186
sernamalala <sernaprograms@gmail.com>
2024-02-17 22:57:27 +0200

    for loop comprehension in python

---
+++ CodeCademy Python Projects/loopComprehension.py
@@ -0,0 +1,14 @@
+# Your code below:
+
+single_digits = [0,1,2,3,4,5,6,7,8,9]
+
+for i in single_digits:
+  print(i)
+
+squares = [squareit**2 for squareit in single_digits]
+
+print(squares)
+
+cubes = [cubeit**3 for cubeit in single_digits]
+
+print(cubes)
